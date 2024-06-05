b59360fdeaad162f894a06227e68e50eaabf4eea
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-06-05 10:35:30 +0530

    Add imp notes regarding auto

--- Session28/09_Solving_ReturnType_Issues.cpp
+++ Session28/09_Solving_ReturnType_Issues.cpp
@@ -11,8 +11,15 @@ int main()
 	double b{ 4.5 };
 	auto t = maximum(a, b);
 	cout << t << endl;
-	auto r = maximum(90, 'h');
+	auto r = maximum(90, 'b');
 	cout << r << endl;
 	// Here the return type will be deduced by the compiler itself
+
+	auto d = maximum<int, char>('o', 'i');
+	cout << d << endl;
 	return 0;
 }
+
+
+// vimp :-  If we are using the auto type deduction then we must provide the function definition before the actual use of the function
+// Matlab upar declare kar diya aur bad me main khatam hone ke baad define kiya toh nhi chalge in case if we are using the auto deduction.
