83532d1f5aa3786cc872e6522da9a20a16b2c693
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-06-05 10:27:48 +0530

    Solving Return Type Issues

---
+++ Session28/09_Solving_ReturnType_Issues.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+template<typename a, typename b> auto maximum(a x, b y)
+{
+	return (x > y) ? x : y;
+}
+int main()
+{
+	cout << "Hello world" << endl;
+	int a{ 5 };
+	double b{ 4.5 };
+	auto t = maximum(a, b);
+	cout << t << endl;
+
+	// Here the return type will be deduced by the compiler itself
+	return 0;
+}
