c4e43e425c19ac7fb1b6cd9f8bf9912ecc4950d8
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-06-10 18:26:19 +0530

    Some imp comments added

--- Session29/Session29.cpp
+++ Session29/Session29.cpp
@@ -45,16 +45,22 @@ t fun(t a, t b) requires integral<t>{
 int main()
 {
     std::cout << "Hello World!\n";
-    cout<<"This is the video for concepts" << endl;
+    cout << "This is the video for concepts" << endl;
     int a{ 1 };
     int b{ 10 };
-    cout<<fun(a, b)<<endl;
+    cout << fun(a, b) << endl;
     float c{ 1.34 };
     float d{ 4.677 };
     char r{ 'b' };
     char s{ 'a' };
-    int sol = fun(a, b)+97;
+    int sol = fun(a, b) + 97;
     cout << sol << endl;
-    
+    return 0;
 }
 
+
+
+// In this session we discussed on the ways in which we can define a function template which is restricted to work only on the integral types of input values. If we will try to use it with the chars then it will work because
+// the chars are convertible to int's but as strings are not convertible to ints we can not use it.
+s
+// We can not use the floats or doubles as the parameters for the function because those can not be connot be converted to int. 
